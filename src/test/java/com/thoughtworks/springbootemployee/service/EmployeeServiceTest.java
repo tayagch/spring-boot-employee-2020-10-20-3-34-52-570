08fcba6505b1923fb0891c8a12438f21a9c7c746
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-21 22:53:49 +0800

    [employee-spring-boot][Raymond Jeyem]Feature: Refactor codes

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -4,7 +4,6 @@ import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.verify;
 import java.util.List;
@@ -13,15 +12,14 @@ import static java.util.Arrays.asList;
 import static org.mockito.Mockito.*;
 
 class EmployeeServiceTest {
-
-    @Mock
+    //Givens
     EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
     Employee employeeRequest = new Employee(1, "junjun", 10, "male", 200 );
+    List<Employee> expectedEmployees = asList(new Employee(),new Employee());
 
     @Test
     void should_get_all_when_get_employees(){
         //GIVEN
-        List<Employee> expectedEmployees = asList(new Employee(),new Employee());
         when(repository.findAll()).thenReturn(expectedEmployees);
         EmployeeService service = new EmployeeService(repository);
         //WHEN
@@ -31,12 +29,12 @@ class EmployeeServiceTest {
     }
     @Test
     void should_create_employee_when_create_given_employee_request(){
-
+        //GIVEN
         when(repository.save(employeeRequest)).thenReturn(employeeRequest);
         EmployeeService employeeService = new EmployeeService(repository);
-
+        //WHEN
         Employee actual = employeeService.create(employeeRequest);
-
+        //THEN
         Assertions.assertEquals(1, actual.getId());
     }
 
@@ -54,27 +52,25 @@ class EmployeeServiceTest {
     void should_get_updated_employeeId_when_update_given_employee_request(){
         //GIVEN
         when(repository.update(1,employeeRequest)).thenReturn(employeeRequest);
-
+        //WHEN
         EmployeeService employeeService = new EmployeeService(repository);
         Employee actual = employeeService.update(1, employeeRequest);
-
+        //THEN
         Assertions.assertEquals(1, actual.getId());
     }
     @Test
     void should_get_employee_with_correct_gender_when_search_given_employee_request(){
         //GIVEN
-        List<Employee> expectedEmployees = asList(new Employee(),new Employee());
         when(repository.getByGender("male")).thenReturn(expectedEmployees);
-
+        //WHEN
         EmployeeService employeeService = new EmployeeService(repository);
         List<Employee> actual = employeeService.getByGender("male");
-
+        //THEN
         Assertions.assertEquals(2, actual.size());
     }
     @Test
     void should_get_correct_page_size_when_given_getPage_employee_request(){
         //GIVEN
-        List<Employee> expectedEmployees = asList(new Employee(),new Employee());
         when(repository.getByPage(1, 5)).thenReturn(expectedEmployees);
         EmployeeService service = new EmployeeService(repository);
         //WHEN
@@ -86,10 +82,10 @@ class EmployeeServiceTest {
     void should_get_employee_with_correct_id_when_search_given_employee_request(){
         //GIVEN
         when(repository.getById(1)).thenReturn(employeeRequest);
-
+        //WHEN
         EmployeeService employeeService = new EmployeeService(repository);
         Employee actual = employeeService.getById(1);
-
+        //THEN
         Assertions.assertEquals(1, actual.getId());
     }
 
