9c4635b3df2fa782fbaf96d961fd903cfe25674a
TAYAGCH <tayagch@oocl.com>
2020-10-24 17:28:46 +0800

    [employee-spring-boot][Christian & Raymond] fix : CompanyServiceTest

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -1,101 +1,114 @@
-//package com.thoughtworks.springbootemployee.service;
-//
-//import com.thoughtworks.springbootemployee.model.Company;
-//import com.thoughtworks.springbootemployee.model.Employee;
-//import com.thoughtworks.springbootemployee.repository.CompanyRepository;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import java.util.List;
-//
-//import static java.util.Arrays.asList;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//class CompanyServiceTest {
-//
-//    //Givens
-//    //should use @Before
-//    CompanyRepository repository = Mockito.mock(CompanyRepository.class);
-//    List<Company> expectedCompanies = asList(new Company(),new Company());
-//    List<Employee> employees = asList(new Employee(), new Employee());
-//    Company companyRequest = new Company("Alibaba",200,employees);
-//
-//    @Test
-//    void should_get_all_when_get_companies(){
-//        //GIVEN
-//        when(repository.findAll()).thenReturn(expectedCompanies);
-//        CompanyService service = new CompanyService(repository);
-//        //WHEN
-//        List<Company> actual = service.getAll();
-//        //THEN
-//        Assertions.assertEquals(2, actual.size());
-//    }
-//
-//    @Test
-//    void should_create_company_when_create_given_company_request(){
-//        //GIVEN
-//        when(repository.save(companyRequest)).thenReturn(companyRequest);
-//        CompanyService companyService = new CompanyService(repository);
-//        //WHEN
-//        Company actual = companyService.create(companyRequest);
-//        //THEN
-//        Assertions.assertEquals("Alibaba", actual.getCompanyName());
-//    }
-//
-//    @Test
-//    void should_get_company_when_search_given_company_request(){
-//        //GIVEN
-//        when(repository.search(200)).thenReturn(companyRequest);
-//        CompanyService companyService = new CompanyService(repository);
-//        //WHEN
-//        Company actual = companyService.findByCompanyId(200);
-//        //THEN
-//        Assertions.assertEquals("Alibaba", actual.getCompanyName());
-//    }
-//
-//    @Test
-//    void should_get_updated_company_when_update_given_company_request(){
-//        //GIVEN
-//        when(repository.update(200,companyRequest)).thenReturn(companyRequest);
-//        CompanyService companyService = new CompanyService(repository);
-//        //WHEN
-//        Company actual = companyService.update(200,companyRequest);
-//        //THEN
-//        Assertions.assertEquals("Alibaba", actual.getCompanyName());
-//    }
-//
-//    @Test
-//    void should_get_delete_company_when_delete_given_company_request() {
-//        //given
-//        CompanyService companyService = new CompanyService(repository);
-//        // when
-//        companyService.delete(200);
-//        //then
-////        verify(repository).delete(200);
-//    }
-//
-//    @Test
-//    void should_get_company_when_getByPage_given_company_request() {
-//        //GIVEN
-//        when(repository.getByPage(1,3)).thenReturn(expectedCompanies);
-//        CompanyService companyService = new CompanyService(repository);
-//        //WHEN
-//        List<Company> companyActual = companyService.getByPage(1,3);
-//        //THEN
-//        Assertions.assertEquals(2, companyActual.size());
-//    }
-//
-//    @Test
-//    void should_get_company_emplyees_when_getEmployees_given_company_request() {
-//        //GIVEN
-//        when(repository.getEmployees(200)).thenReturn(companyRequest.getEmployees());
-//        CompanyService companyService = new CompanyService(repository);
-//        //WHEN
-//        List<Employee> actual = companyService.getEmployees(200);
-//        //THEN
-//        Assertions.assertEquals(companyRequest.getEmployees(), actual);
-//    }
-//
-//}
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CompanyServiceTest {
+
+    //Givens
+    //should use @Before
+    CompanyRepository repository = Mockito.mock(CompanyRepository.class);
+    EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
+    List<Company> expectedCompanies = asList(new Company(),new Company());
+    List<Employee> employees = asList(new Employee(), new Employee());
+    Company companyRequest = new Company(1,"Alibaba",employees);
+
+    @Test
+    void should_get_all_when_get_companies(){
+        //GIVEN
+        when(repository.findAll()).thenReturn(expectedCompanies);
+        CompanyService service = new CompanyService(repository,employeeRepository);
+        //WHEN
+        List<Company> actual = service.getAll();
+        //THEN
+        assertEquals(2, actual.size());
+    }
+
+    @Test
+    void should_create_company_when_create_given_company_request(){
+        //GIVEN
+        when(repository.save(companyRequest)).thenReturn(companyRequest);
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        //WHEN
+        Company actual = companyService.create(companyRequest);
+        //THEN
+        assertEquals("Alibaba", actual.getCompanyName());
+    }
+
+    @Test
+    void should_get_company_when_search_given_company_request(){
+        //GIVEN
+        when(repository.findById(1)).thenReturn(java.util.Optional.ofNullable(companyRequest));
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        //WHEN
+        Company actual = companyService.findByCompanyId(1);
+        //THEN
+        assertEquals("Alibaba", actual.getCompanyName());
+    }
+
+    @Test
+    void should_get_updated_company_when_update_given_company_request(){
+        //GIVEN
+        when(repository.findById(1)).thenReturn(Optional.ofNullable(companyRequest));
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        companyService.findByCompanyId(1);
+        //WHEN
+
+        companyService.update(1,companyRequest);
+        //THEN
+        verify(repository).save(companyRequest);
+    }
+
+    @Test
+    void should_get_delete_company_when_delete_given_company_request() {
+        //given
+        when(repository.save(companyRequest)).thenReturn(companyRequest);
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        // when
+        companyService.delete(1);
+        //then
+        assertEquals(0,companyService.getAll().size());
+    }
+
+    @Test
+    void should_get_company_when_getByPage_given_company_request() {
+        //GIVEN
+        Page<Company> companyPage = new PageImpl<Company>(expectedCompanies);
+        Pageable pageable = PageRequest.of(1,3);
+        when(repository.findAll(pageable)).thenReturn(companyPage);
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        //WHEN
+        List<Company> companyActual = companyService.getByPage(1,3);
+        //THEN
+        assertEquals(2, companyActual.size());
+    }
+
+    @Test
+    void should_get_company_emplyees_when_getEmployees_given_company_request() {
+        //GIVEN
+        when(repository.findById(1)).thenReturn(Optional.ofNullable(companyRequest));
+        CompanyService companyService = new CompanyService(repository,employeeRepository);
+        //WHEN
+        List<Employee> actual = companyService.getEmployees(1);
+        //THEN
+        assertEquals(companyRequest.getEmployees(), actual);
+    }
+
+}
